29143cb6a6457894e387b3f45c5cd3368e29a847
Light <ccxxaabb@qq.com>
2019-07-24 18:13:26 +0800

    品优购实战提交第二次测试提交，cx 折线图与饼状图 日期处于写死状态，稍后会进行增加前端日期列表选择项

---
+++ pinyougou-prent/pinyougou-shop-web/src/main/webapp/js/controller/pictureController.js
@@ -0,0 +1,25 @@
+var app = new Vue({
+    //1.与前端对应的标识
+    el:"#app",
+    //2.放置属性的地方
+    data:{
+        startTime_year:'',
+        startTime_month:'',
+        startTime_day:'',
+        endTime_year:'',
+        endTime_month:'',
+        endTime_day:''
+
+    },
+    //3.放置方法的地方
+    methods: {
+
+    },
+
+    //4.钩子函数，一些显示数据可初始化的地方
+    created:function () {
+        
+    }
+
+
+});
