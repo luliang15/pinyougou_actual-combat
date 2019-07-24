782a4f399ce74508890622684b304ae2b9c316b1
yang <1349638107@qq.com>
2019-07-24 20:58:11 +0800

    杨家盛7.24我的收藏到购物车一半需求

--- pinyougou-prent/pinyougou-user-web/src/main/webapp/js/controller/userController.js
+++ pinyougou-prent/pinyougou-user-web/src/main/webapp/js/controller/userController.js
@@ -9,6 +9,8 @@
         ids:[],
         searchEntity:{},
         loginName:'',  //获取到的用户名
+        num: 1,
+        sku:skuList[0],
     },
     methods: {
 
@@ -65,9 +67,36 @@
             }).catch(function (error) {
                 console.log(error);
             });
+        },//循环遍历SKU的列表数组
+        // 判断 当前的变量的值是否在数组中存在,如果存在,将对应的数组的元素赋值给变量sku
+
+        search:function () {
+            for(var i=0;i<skuList.length;i++){
+                //{"id":14383881,"title":"iphonex60 移动3G 16G","price":0.01,spec:{"网络":"移动3G","机身内存":"16G"}}
+                var obj = skuList[i];//
+                if(JSON.stringify(this.specificationItems)==JSON.stringify(obj.spec)){
+                    this.sku=obj;
+                    break;
+                }
+            }
         },
+        //添加购物车
+        addGoodsToCart:function () {
+            alert("addGoodsToCart")
+            axios.get('http://localhost:9107/cart/addGoodsToCartList.shtml?itemId='+this.sku.id+'&num='+this.num).
+            then(
+                function (response) {//response.data=result
+                    if(response.data.success){
+                        window.location.href="http://localhost:9107/cart.html";
+                    }else{
+                        alert("错了");
+                    }
+                }
+            )
+        }
 
     },
+
     //钩子函数 初始化了事件和
     created: function () {
 
